4c5574f1b7b9a3963d3b9dfaf75674ae7d5bd1d7
Félix Francisco <82882273+felix856@users.noreply.github.com>
2022-06-02 07:41:13 -0300

    Create tela_pontuacao.java
    pontuacaodeclara

---
+++ tela_pontuacao.java
@@ -0,0 +1,13 @@
+package com.example.sa;
+
+public class Pontuações {
+    int pontuação_1;
+    int pontuação_2;
+    int pontuação_3;
+    int pontuação_4;
+    int pontuação_5;
+    int pontuação_6;
+    int pontuação_7;
+    int pontuação_8;
+
+}
